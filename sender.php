b2dadc8561d5ccd1b5aabc1794c2732994b5ee3a
Vavadjan <vavadjan@gmail.com>
2023-08-24 14:44:16 +0300

    xxx

--- sender.php
+++ sender.php
@@ -5,11 +5,11 @@
     $email = $_POST['email'];
     $text = $_POST['text'];
 
-	$to = "denis.koblya@gmail.com"; 
+	$to = "vavadjan@gmail.com"; 
 	$date = date ("d.m.Y"); 
 	$time = date ("h:i");
 	$from = $email;
-	$subject = "Заявка c сайта";
+	$subject = "Заявка c сайта SpartaJobs";
 
 	
 	$msg="
